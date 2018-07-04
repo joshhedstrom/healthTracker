1e59d8af68ddea53236305acbc04776663774d24
Lucas Jensen <lucasjensen56@gmail.com>
2018-07-04 11:53:44 -0500

    added some style to daily progress on dashboard. Added buttons for future functionality

--- client/src/components/DashBoard/DailyProgress/index.js
+++ client/src/components/DashBoard/DailyProgress/index.js
@@ -9,6 +9,9 @@ import Typography from '@material-ui/core/Typography';
 import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import SkipNextIcon from '@material-ui/icons/SkipNext';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import Icon from '@material-ui/core/Icon';
 
 const styles = theme => ({
   card: {
@@ -21,17 +24,31 @@ const styles = theme => ({
   content: {
     flex: '1 0 auto',
   },
+  button: {
+    margin: theme.spacing.unit,
+  },
+  input: {
+    display: 'none',
+  },
+  divStyle: {
+    padding: 14
+  }
 });
 
 function MediaControlCard(props) {
   const { classes, theme } = props;
 
   return (
-    <div>
+    <div className={classes.divStyle}>
       <Card className={classes.card}>
         <div className={classes.details}>
           <CardContent className={classes.content}>
             <Typography variant="headline">Water Intake</Typography>
+             <Button variant="contained" color="primary" aria-label="add" className={classes.button}>
+              Add
+              <AddIcon />
+           </Button>
+
           </CardContent>
           
         </div>
@@ -42,6 +59,10 @@ function MediaControlCard(props) {
         <div className={classes.details}>
           <CardContent className={classes.content}>
             <Typography variant="headline">Nutrition</Typography>
+            <Button variant="contained" color="primary" aria-label="add" className={classes.button}>
+              Add
+              <AddIcon />
+           </Button>
           </CardContent>
         </div>
       </Card>
@@ -50,6 +71,10 @@ function MediaControlCard(props) {
         <div className={classes.details}>
           <CardContent className={classes.content}>
             <Typography variant="headline">Exercise</Typography>
+            <Button variant="contained" color="primary" aria-label="add" className={classes.button}>
+              Add
+              <AddIcon />
+           </Button>
           </CardContent>
         </div>
       </Card>
@@ -58,6 +83,10 @@ function MediaControlCard(props) {
         <div className={classes.details}>
           <CardContent className={classes.content}>
             <Typography variant="headline">Weight</Typography>
+            <Button variant="contained" color="primary" aria-label="add" className={classes.button}>
+              Add
+              <AddIcon />
+           </Button>
           </CardContent>
         </div>
       </Card>
