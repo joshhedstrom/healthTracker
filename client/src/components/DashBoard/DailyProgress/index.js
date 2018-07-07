f03e065c3cd193705ec8dfdb247a51a35ffd4c20
Lucas Jensen <lucasjensen56@gmail.com>
2018-07-07 10:24:13 -0500

    started working on meu drawer

--- client/src/components/DashBoard/DailyProgress/index.js
+++ client/src/components/DashBoard/DailyProgress/index.js
@@ -3,16 +3,10 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
-import SkipNextIcon from '@material-ui/icons/SkipNext';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import InfoIcon from '@material-ui/icons/Info';
-import Icon from '@material-ui/core/Icon';
 import Grid from '@material-ui/core/Grid';
 
 const styles = theme => ({
--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -7,6 +7,8 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import MenuDrawer from '../MenuDrawer'
+
 
 const styles = {
   root: {
@@ -22,6 +24,8 @@ const styles = {
 };
 
 function ButtonAppBar(props) {
+
+
   const { classes } = props;
   return (
     <div className={classes.root}>
@@ -37,8 +41,24 @@ function ButtonAppBar(props) {
         </Toolbar>
       </AppBar>
     </div>
+
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ButtonAppBar.propTypes = {
   classes: PropTypes.object.isRequired,
