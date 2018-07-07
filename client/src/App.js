d3389b3cd13cac031c16615dd3e0f624105d554b
Lucas Jensen <lucasjensen56@gmail.com>
2018-07-07 10:25:11 -0500

    small changes for code

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from './components/NavBar';
 import DashBoard from "./pages/DashBoard"
+import Login from "./pages/Login"
 
 import './App.css';
 
@@ -11,7 +12,8 @@ const App = () => (
       <NavBar />
       <Switch>
        <Route exact path="/dashboard" component={DashBoard} />
-       
+       <Route exact path="/login" component={Login} />
+
       </Switch>
     </div>
   </Router>
--- client/src/pages/DashBoard/DashBoard.js
+++ client/src/pages/DashBoard/DashBoard.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import UserInfoCard from '../../components/DashBoard/UserInfoCard';
 import DailyProgress from '../../components/DashBoard/DailyProgress';
 
