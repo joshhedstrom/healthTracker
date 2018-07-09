15ae2ebced3ec8e00add6848ab321e3838f50987
lwerner27 <lwerner27@gmail.com>
2018-07-09 15:57:23 -0500

    fixed models and controllers

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 module.exports = {
     User: require("./User"),
-    Day: require("./Day"),
-    Nutrition: require("./Nutrition"),
-    Exercise: require("./Exercise"),
-    Water: require("./Water")
+    Day: require("./Day")
+    // Nutrition: require("./Nutrition"),
+    // Exercise: require("./Exercise"),
+    // Water: require("./Water")
 };
   
--- routes/api/healthTracker.js
+++ routes/api/healthTracker.js
@@ -1,49 +1,19 @@
-<<<<<<< HEAD
 const router = require("express").Router();
-const healthTrackerController = require("../../controllers/healthTrackerController");
+const db = require("../../controllers");
 
 // Matches with "/api/healthTracker"
-router.route("/")
-//user
-.get(healthTrackerController.findAllUsers)
-.post(healthTrackerController.createUser)
-//exercise  
-.get(healthTrackerController.findAllExercises)
-.post(healthTrackerController.createExercise);
+router.post("/newUser", (req, res) => {
+    db.User.createUser(req, res)
+})
 
-// Matches with "/api/healthTracker/:id"
-router
-  .route("/:id")
-  //user
-  .get(healthTrackerController.findUserById)
-  .put(healthTrackerController.updateUser)
-  .delete(healthTrackerController.removeUser)
-  //water
-  .put(healthTrackerController.updateWater)
-  //nutrition
-  .put(healthTrackerController.updateNutrition)
-  //day
-  .put(healthTrackerController.updateDay)
-  //exercise
-  .put(healthTrackerController.updateExercise);
+router.post("/newDay", (req, res) => {
+  console.log(req.body)
+  db.Day.createDay(req, res)
+})
+
+router.get("/:id", (req, res) => {
+  db.User.findUserById(req, res)
+})
+
 
 module.exports = router;
-=======
-const express = require("express")
-
-const router = express.Router()
-
-const db = require("../../models");
-
-router.post("/", (req, res) => {
-    db.User
-        .create(req.body)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err))
-})
-
-router.get("/:username", (req, res) => {
-})
-
-module.exports = router
->>>>>>> 9c522cd62cd26f37ca11271c1305a6bf3e4744cf
