84edbbf053f779c5c7f9f8bfc904cbf928f8c604
lwerner27 <lwerner27@gmail.com>
2018-07-09 14:14:08 -0500

    separated the controllers out into individual files

---
+++ controllers/userController.js
@@ -0,0 +1,33 @@
+const db = require("../models")
+
+module.exports = {
+    // User controllers
+    findUserById: function(req, res) {
+        db.User
+        .findById(req.params.id)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+
+    createUser: function(req, res) {
+        db.User
+        .create(req.body)
+        .then(userModel => res.json(userModel))
+        .catch(err => res.status(422).json(err));
+    },
+
+    updateUser: function(req, res) {
+        db.User
+        .findOneAndUpdate({ _id: req.params.id }, req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    
+    removeUser: function(req, res) {
+        db.User
+        .findById({ _id: req.params.id })
+        .then(dbModel => dbModel.remove())
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+}
