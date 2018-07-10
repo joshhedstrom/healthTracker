cb83fa401b3f8bccd449f3a06192c0e8f26dc019
lwerner27 <lwerner27@gmail.com>
2018-07-09 20:26:26 -0500

    fixed some problems in the dayController

--- controllers/dayController.js
+++ controllers/dayController.js
@@ -6,11 +6,12 @@ module.exports = {
         db.Day
         .create(req.body)
         .then(dbModel => {
-            db.User.findByIdAndUpdate({username: req.body.username})
+            db.User.findByIdAndUpdate({_id: req.body.id})
             .then((userModel) => {
                 userModel.days.push(dbModel._id)
                 userModel.save()
             })
+        return res.send("Day has been added")
         })
         .catch(err => res.status(422).json(err));
     },
