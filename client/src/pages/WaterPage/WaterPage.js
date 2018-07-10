b47d27a97d8d8ecee6f6da26cb5dee9bac1e5034
Lucas Jensen <lucasjensen56@gmail.com>
2018-07-09 19:30:51 -0500

    started working on the water page

---
+++ client/src/pages/WaterPage/WaterPage.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import WaterGoalCard from '../../components/WaterGoalCard';
+
+class WaterGoal extends Component {
+
+
+
+render() {
+	return (
+		<div>
+			<WaterGoalCard />
+	    </div>
+
+	)
+}
+
+};
+
+export default WaterGoal;
