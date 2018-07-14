a881000797c4aeffd25b507a1738fbf6898105c0
lwerner27 <lwerner27@gmail.com>
2018-07-14 13:20:40 -0500

    Displaying users data on the dashboard Page

--- client/src/pages/DashBoard/DashBoard.js
+++ client/src/pages/DashBoard/DashBoard.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
+import DashBoardComponent from '../../components/DashBoard';
 
-import DashBoardCompontent from '../../components/DashBoard';
 import { Redirect } from 'react-router-dom';
+import axios from "axios"
 
 class DashBoard extends Component {
   state = {
+    userId: "",
+    firstName: "",
+    lastName: "",
     redirect: false,
     waterIntake: 0,
     nutritionPoints: 0,
-    exerciseHrs: 0,
-    weightCurrent: 0
+    exerciseMins: 0,
+    currentWeight: 0
   };
+
+  totalExerciseMinutes(arr) {
+    let totalMinutes = 0
+    for (let i = 0; i < arr.length; i++ ) {
+      totalMinutes = totalMinutes + arr[i].duration
+    }
+
+    if (totalMinutes) {
+      return totalMinutes
+    } else {
+      return 0
+    }
+  }
+
+  componentDidMount() {
+    this.setState({userId: localStorage.getItem("userId")})
+
+    let url = `http://localhost:3001/api/healthtracker/user/${localStorage.getItem('userId')}`
+    axios.defaults.headers.common['Authorization'] = localStorage.getItem(
+      'jwtToken'
+    );
+    axios
+    .get(url)
+    .then(res => {
+      this.setState({
+        firstName: res.data.name,
+        lastName: res.data.name,
+        waterIntake: res.data.days[0].water,
+        nutritionPoints: res.data.days[0].nutrition,
+        exerciseMins: this.totalExerciseMinutes(res.data.days[0].exercises),
+        currentWeight: res.data.weight
+        
+      })
+    })
+  }
 
   renderRedirect = () => {
     if (!localStorage.getItem('jwtToken')) {
@@ -22,8 +61,7 @@ class DashBoard extends Component {
     return (
       <div>
         {this.renderRedirect()}
-        <DashBoardCompontent />
-
+        <DashBoardComponent water={this.state.waterIntake} nutrition={this.state.nutritionPoints} weight={this.state.currentWeight} exercise={this.state.exerciseMins} />
       </div>
     );
   }
