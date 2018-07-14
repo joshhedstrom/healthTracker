959b55a1f87fd0c426f7bb5e4121f8950785660b
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-07-14 12:44:40 -0500

    changed to grid system

--- client/src/components/WaterGoalCard/index.js
+++ client/src/components/WaterGoalCard/index.js
@@ -13,115 +13,124 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Grid from '@material-ui/core/Grid';
 
 const styles = theme => ({
   root: {
     ...theme.mixins.gutters(),
     paddingTop: theme.spacing.unit * 2,
     paddingBottom: theme.spacing.unit * 2,
-    margin: 10,
+    margin: 10
   },
   textField: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
-    width: 200,
+    width: 200
   },
   container: {
     display: 'flex',
-    flexWrap: 'wrap',
+    flexWrap: 'wrap'
   },
   expansionPanelStyle: {
-    margin: 11,
+    margin: 11
   }
-
-
 });
 
-
 class PaperSheet extends React.Component {
-
   state = {
     glasses: ''
-
   };
 
   handleChange = name => event => {
     this.setState({
-      [name]: event.target.value,
-    })
+      [name]: event.target.value
+    });
   };
 
-
-
-render () {
-  const { classes } = this.props;
+  render() {
+    const { classes } = this.props;
 
     return (
       <div>
         <Paper className={classes.root} elevation={1}>
-          <Typography variant="display1">
-            Water Goal
-          </Typography>
-          <Typography>
-            Drink at least 8 fluid ounces each day. 
-            One Glass counts as one fluid ounce 
-          </Typography>
-          <Typography variant="title">
-            Add Water
-          </Typography>
-          <Typography variant="subheading">
-            Today's Progress
-          </Typography>
-          <Table>
-            <TableBody>
-              <TableRow>
-                <TableCell>3 Glasses</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
+          <Grid container spacing={24}>
+            <Grid item xs={12}>
+              <Typography variant="display1">Water Goal</Typography>
 
-          <Button variant="outlined" size="small">+1 Glasses</Button>
-          <Button variant="outlined" size="small">+3 Glasses</Button>
-          <Button variant="outlined" size="small">+6 Glasses</Button>
+              <Typography>
+                Drink at least 8 fluid ounces each day. One Glass counts as one
+                fluid ounce
+              </Typography>
 
-           <form className={classes.container} noValidate autoComplete="off">
-              <TextField
-              id="addGlasses"
-              label="Enter Water"
-              value={this.state.glasses}
-              onChange={this.handleChange('glasses')}
-              type="number"
-              className={classes.textField}
-              margin="normal"
-            />
-            </form>
-          <Button variant="contained">Submit</Button>
+              <Typography variant="title">Add Water</Typography>
+
+              <Typography variant="subheading">Today's Progress</Typography>
+
+              <Table>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>3 Glasses</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Grid>
+            <Grid item xs={4}>
+              <Button variant="outlined" size="small">
+                +1 Glasses
+              </Button>
+            </Grid>
+            <Grid item xs={4}>
+              <Button variant="outlined" size="small">
+                +3 Glasses
+              </Button>
+            </Grid>
+            <Grid item xs={4}>
+              <Button variant="outlined" size="small">
+                +6 Glasses
+              </Button>
+            </Grid>
+            <Grid item xs={12}>
+              <form className={classes.container} noValidate autoComplete="off">
+                <TextField
+                  id="addGlasses"
+                  label="Enter Water"
+                  fullWidth
+                  value={this.state.glasses}
+                  onChange={this.handleChange('glasses')}
+                  type="number"
+                  margin="normal"
+                />
+              </form>
+            </Grid>
+            <Grid item xs={12}>
+              <Button variant="contained">Submit</Button>
+            </Grid>
+          </Grid>
         </Paper>
-      
-          <ExpansionPanel  className={classes.expansionPanelStyle} defaultExpanded>
-          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <div className={classes.column}>
-              <Typography className={classes.heading}>History</Typography>
-            </div>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails className={classes.details}>
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <ExpansionPanel
+              className={classes.expansionPanelStyle}
+              defaultExpanded
+            >
+              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                <div className={classes.column}>
+                  <Typography className={classes.heading}>History</Typography>
+                </div>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails className={classes.details}>
                 graph goes here
-          
-          </ExpansionPanelDetails>
-
-         
-        </ExpansionPanel>
-
-
-
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </Grid>
+        </Grid>
       </div>
-  );
-
+    );
   }
 }
 
 PaperSheet.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(PaperSheet);
