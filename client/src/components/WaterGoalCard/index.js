3af61ba0949f4a185d9de984b07ef1780b5d2741
unknown <bubbastinx@gmail.com>
2018-07-14 13:36:32 -0600

    added barChart pieChart added different datasets for scale

--- client/src/components/WaterGoalCard/index.js
+++ client/src/components/WaterGoalCard/index.js
@@ -13,6 +13,7 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChartsBar from './../ChartsBar';
 
 const styles = theme => ({
   root: {
@@ -105,7 +106,7 @@ render () {
             </div>
           </ExpansionPanelSummary>
           <ExpansionPanelDetails className={classes.details}>
-                graph goes here
+                <ChartsBar />
           
           </ExpansionPanelDetails>
 
