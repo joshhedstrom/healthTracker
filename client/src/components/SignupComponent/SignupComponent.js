f206be5b9f0a4085424cd0d75abe867a15c93fc0
Lucas Jensen <lucasjensen56@gmail.com>
2018-07-21 11:01:16 -0500

    started foundation for alert

--- client/src/components/SignupComponent/SignupComponent.js
+++ client/src/components/SignupComponent/SignupComponent.js
@@ -6,6 +6,14 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import Slide from '@material-ui/core/Slide';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+
 
 const styles = theme => ({
   root: {
@@ -18,96 +26,133 @@ const styles = theme => ({
   }
 });
 
-function Signup(props) {
-  const { classes } = props;
 
-  return (
-    <div>
-      <Paper className={classes.root} elevation={1}>
-        <Typography variant="headline" component="h3">
-          Create Account
-        </Typography>
-        <h4>{props.message}</h4>
-        <Grid container spacing={24}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              id="firstName"
-              label="First Name (required)"
-              className={classes.textField}
-              margin="normal"
-              fullWidth
-              onChange={props.firstNameAction}
-            />
+
+class Signup extends React.Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <Paper className={classes.root} elevation={1}>
+          <Typography variant="headline" component="h3">
+            Create Account
+          </Typography>
+          <h4>{this.props.message}</h4>
+          <Grid container spacing={24}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="firstName"
+                label="First Name (required)"
+                className={classes.textField}
+                margin="normal"
+                fullWidth
+                onChange={this.props.firstNameAction}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="lastName"
+                label="Last Name (required)"
+                className={classes.textField}
+                margin="normal"
+                fullWidth
+                onChange={this.props.lastNameAction}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="username"
+                label="Username (required)"
+                className={classes.textField}
+                margin="normal"
+                fullWidth
+                onChange={this.props.usernameAction}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="weight"
+                label="Weight (optional)"
+                className={classes.textField}
+                margin="normal"
+                fullWidth
+                onChange={this.props.weightAction}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="password"
+                label="Password"
+                className={classes.textField}
+                type="password"
+                autoComplete="current-password"
+                margin="normal"
+                fullWidth
+                onChange={this.props.passwordAction}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="passwordConfirmation"
+                label="Confirm Password"
+                className={classes.textField}
+                type="password"
+                autoComplete="current-password"
+                margin="normal"
+                fullWidth
+                onChange={this.props.passwordConfirmAction}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                size="large"
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                onClick={this.props.submitAction}
+              >
+                Submit
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              id="lastName"
-              label="Last Name (required)"
-              className={classes.textField}
-              margin="normal"
-              fullWidth
-              onChange={props.lastNameAction}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              id="username"
-              label="Username (required)"
-              className={classes.textField}
-              margin="normal"
-              fullWidth
-              onChange={props.usernameAction}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              id="weight"
-              label="Weight (optional)"
-              className={classes.textField}
-              margin="normal"
-              fullWidth
-              onChange={props.weightAction}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              id="password"
-              label="Password"
-              className={classes.textField}
-              type="password"
-              autoComplete="current-password"
-              margin="normal"
-              fullWidth
-              onChange={props.passwordAction}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              id="passwordConfirmation"
-              label="Confirm Password"
-              className={classes.textField}
-              type="password"
-              autoComplete="current-password"
-              margin="normal"
-              fullWidth
-              onChange={props.passwordConfirmAction}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              size="large"
-              variant="contained"
-              color="primary"
-              className={classes.button}
-              onClick={props.submitAction}
-            >
-              Submit
+        </Paper>
+
+         <Dialog
+          open={this.props.open}
+          onClose={this.props.handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Let Google help apps determine location. This means sending anonymous location data to
+              Google, even when no apps are running.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.props.handleClose} color="primary">
+              Disagree
             </Button>
-          </Grid>
-        </Grid>
-      </Paper>
-    </div>
-  );
+            <Button onClick={this.props.handleClose} color="primary" autoFocus>
+              Agree
+            </Button>
+          </DialogActions>
+        </Dialog>
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+    );
+  }
 }
 
 Signup.propTypes = {
