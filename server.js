dc0c337d817ec0ecdd515d64624918e11da20e37
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-07-23 18:52:27 -0500

    cleaned up file

--- server.js
+++ server.js
@@ -7,27 +7,15 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const morgan = require('morgan')
 
-// Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// Serve up static assets (usually on heroku)
 app.use(express.static(path.join(__dirname, 'client/build')));
-process.env.NODE_ENV = 'production'
-if (process.env.NODE_ENV === "production") {
-}
-// Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reacthealthtracker");
-
-// mongoose.connect(process.env.MONGODB_URI, (err) => {
-// 	console.log(err || `Connected to MongoDB.`)
-// })
 
 app.use(morgan('dev'))
 
-// Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
