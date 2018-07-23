6acc05b6f3f25cf35c5f41ee9a82e49b7870dd81
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-07-23 10:02:49 -0500

    express using build folder

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const morgan = require('morgan')
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
+app.use(express.static("client/build"));
 process.env.NODE_ENV = 'production'
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
