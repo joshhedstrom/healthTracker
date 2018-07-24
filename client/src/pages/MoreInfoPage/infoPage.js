3a25ccd0787cce13f20742974b336ed7aba8a97a
Lucas Jensen <lucasjensen56@gmail.com>
2018-07-23 19:29:24 -0500

    added More info page

---
+++ client/src/pages/MoreInfoPage/infoPage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import MoreInfo from '../../components/MoreInfo';
+import { Redirect } from 'react-router-dom';
+
+class MoreInfoPage extends Component {
+
+
+
+  render() {
+    return (
+      <div>
+        <MoreInfo />
+  
+      </div>
+    );
+  }
+}
+
+export default MoreInfoPage;
