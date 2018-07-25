d605d238d3b35b6da2e394afa21678fe80cf3c08
Lucas Jensen <lucasjensen56@gmail.com>
2018-07-25 18:51:32 -0500

    lite changes

--- client/src/components/ChartsPie/Charts.js
+++ client/src/components/ChartsPie/Charts.js
@@ -23,10 +23,8 @@
                  backgroundColor: [
                     '#63c5e4',
                     '#825eb9b5',
-                    '#10953bb8',
-                    'red',
-                    '#63c5e4',
-                    'pink'
+                    '#10953bb8'
+                    
                 ]
             }],
             options: {
